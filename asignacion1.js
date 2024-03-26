90c15583eb07c2de254bd355f3a486c4b8610490
diegomkxc <162450648+diegomkxc@users.noreply.github.com>
2024-03-26 10:58:51 -0300

    Add files via upload

---
+++ asignacion1.js
@@ -0,0 +1,15 @@
+// declarando las variables string
+let Saludos = "bienvenidoh a otra de esta monda"
+let Intro = "hoy vamo a jugah fornite"
+
+let Nombre = "Diego"
+let Apellido = "Rojas"
+let Usuario = "misterlink"
+
+// declarando los intergers
+let Edad = 16
+let Año = 2007
+let Estatura = 1.79
+
+// declarando arreglos
+let arreglos = ["bienvenidoh a otra de esta monda", "hoy vamo a jugah fornite"]
